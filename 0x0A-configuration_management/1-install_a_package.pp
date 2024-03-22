af167aa318e21be9390843337fd11cbaf63cef18
Yared91 <ykebede010@gmail.com>
2024-03-22 08:19:20 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# Installs flask
+# Installs puppet-lint
 
-package {'flask':
-        ensure   => '2.1.0',
-        provider => 'pip3',
+package {'puppet-lint':
+        ensure   => '2.1.1',
+        provider => 'gem',
 }

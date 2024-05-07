8fb95194106b2b528f30dc0111d8905119a56668
Yared91 <ykebede010@gmail.com>
2024-05-07 02:42:56 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,15 +10,15 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
+                v1.0.0 (by /u/firdaus_cartoon_jr)"
+                }
     params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
     response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+            allow_redirects=False)
     if response.status_code == 404:
         return None
 
